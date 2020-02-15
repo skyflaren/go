2df73ac562540726d73ae933717dde3c71f889aa
RonaldYou <60042404+RonaldYou@users.noreply.github.com>
2020-02-15 07:10:54 -0500

    Add files via upload

---
+++ Nickname.kt
@@ -0,0 +1,12 @@
+package com.example.introscreen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Nickname:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.setting_nickname)
+    }
+
+}
